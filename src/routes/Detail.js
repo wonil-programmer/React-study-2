18ab9bfcafae7f53d990535e97aca2a362084094
원일 허 <70367717+wonil-programmer@users.noreply.github.com>
2023-07-14 19:59:18 +0900

    React: fix parameter error in useEffect

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,15 +1,20 @@
-import { useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
   const { id } = useParams();
-  const getMovie = async () => {
-    await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}
-    `).json();
-  };
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const getMovie = useCallback(async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  }, [id]);
   useEffect(() => {
     getMovie();
-  }, []);
+  }, [getMovie]);
   return <h1>Detail</h1>;
 }
 
