4c92fdcf04ff64638833c3a252c156496c7d0af4
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-07-14 22:06:27 +0900

    React: test..

--- src/MovieApp.js
+++ src/MovieApp.js
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
         <Route
-          path={`${process.env.PUBLIC_URL}//movie/:id`}
+          path={`${process.env.PUBLIC_URL}/movie/:id`}
           element={<Detail />}
         />
       </Routes>
